251060ff4edc3261d6ac64d169da415611db0f18
Victor Tarasevich <v.tarasevitch@invento.by>
2025-03-07 14:28:22 +0300

    --wip-- [skip ci]

--- datahub-web-react/src/app/searchV2/filtersPrototype/filters/EntityTypeFilter/EntityTypeFilter.tsx
+++ datahub-web-react/src/app/searchV2/filtersPrototype/filters/EntityTypeFilter/EntityTypeFilter.tsx
@@ -57,6 +57,7 @@ export default function EntityTypeFilter({
             showSearch
             showCount
             shouldManuallyUpdate
+            selectLabelProps={{ variant: 'labeled', label: 'Types' }}
         />
     );
 }
