719db7d4537ec562008c3b042e06cbb431ae31e5
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-04-11 11:05:03 +0300

    feat(searchBarAutocomplete): add possibility to configure api variant to use in search bar

--- datahub-web-react/src/app/searchV2/useSearchBarData.ts
+++ datahub-web-react/src/app/searchV2/useSearchBarData.ts
@@ -2,7 +2,7 @@ import {
     useGetAutoCompleteMultipleResultsLazyQuery,
     useGetSearchResultsForMultipleTrimmedLazyQuery,
 } from '@src/graphql/search.generated';
-import { AndFilterInput, Entity, EntityType, FacetMetadata } from '@src/types.generated';
+import { AndFilterInput, Entity, EntityType, FacetMetadata, SearchBarApi } from '@src/types.generated';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDebounce } from 'react-use';
 import { FieldToAppliedFieldFiltersMap } from './filtersV2/types';
@@ -28,7 +28,7 @@ export type SearchResponse = {
     facets?: FacetMetadata[];
     entities?: Entity[];
     loading?: boolean;
-    searchAPIVariant?: string;
+    searchAPIVariant?: SearchBarApi;
 };
 
 const SEARCH_API_RESPONSE_MAX_ITEMS = 20;
@@ -114,7 +114,7 @@ export const useSearchBarData = (
     query: string,
     appliedFilters: FieldToAppliedFieldFiltersMap | undefined,
     viewUrn: string | undefined | null,
-    searchAPIVariant: 'searchAcrossEntitiesAPI' | 'autocompleteAPI' | undefined,
+    searchAPIVariant: SearchBarApi | undefined,
     enabled: boolean,
 ): SearchResponse => {
     const [debouncedQuery, setDebouncedQuery] = useState<string>('');
@@ -123,9 +123,9 @@ export const useSearchBarData = (
 
     const api = useMemo(() => {
         switch (searchAPIVariant) {
-            case 'searchAcrossEntitiesAPI':
+            case SearchBarApi.SearchAcrossEntities:
                 return searchAPI;
-            case 'autocompleteAPI':
+            case SearchBarApi.AutocompleteForMultiple:
                 return autocompleteAPI;
             default:
                 return autocompleteAPI;
